2d0521366304d17267ed8cdcab6e7673f06c5126
Adil Bensalah <adilbensalah133@gmail.com>
2023-02-26 18:02:35 +0100

    nav_bar_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
-import AppelNavbar from "./Components/Navbar/AppelNavbar"
-import './App.css'
+import AppelNavbar from "./Components/Navbar/AppelNavbar";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <AppelNavbar/>
+      <AppelNavbar />
     </>
   );
 }
--- src/Components/Navbar/AppelNavbar.jsx
+++ src/Components/Navbar/AppelNavbar.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AppelNavbar.css";
 import appelLogo from "./assets/appel-logo.png";
 import serch from "./Assets/serch.jpg";
 import panier from "./Assets/panier.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function AppelNavBar() {
+  const [toggle, setToggle] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  console.log(toggle);
+  const handelResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  console.log(width);
+  useEffect(() => {
+    window.addEventListener("resize", handelResize);
+
+    return () => {
+      window.removeEventListener("resize", handelResize);
+    };
+  }, []);
+
+  const handelClick = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <>
       <nav>
         <img src={appelLogo} alt="appel" className="appel-logo" />
-        <div className="liste">
+
+        <div
+          className={toggle || width > 770 ? "liste liste-toggle " : "liste"}
+        >
           <span>
-            <a href="#">Store</a>
+            <a href="#">Store </a>
           </span>
           <span>
             <a href="#">Mac</a>
@@ -37,12 +62,19 @@ export default function AppelNavBar() {
           <span>
             <a href="#">Accessoires</a>
           </span>
-          <span>
-            <a href="#">Assistance</a>
-          </span>
         </div>
-        <img src={serch} alt="serch" className="serch" />
-        <img src={panier} alt="panier" className="panier" />
+
+        <div className="btns">
+          <img src={serch} alt="serch" className="serch" />
+          <img src={panier} alt="panier" className="panier" />
+        </div>
+        <button
+          onClick={handelClick}
+          className={toggle ? "toggle-btn" : "hight-btn"}
+        >
+          <span className={toggle ? " line active-l1 " : "line l1"}></span>
+          <span className={toggle ? " line active-l2" : "line l2"}></span>
+        </button>
       </nav>
     </>
   );
